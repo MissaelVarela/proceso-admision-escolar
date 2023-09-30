cc5c22078174da3a0aaf0fd21cc2269cb2d49bc7
Missael Varela <misselvarelayocupicio@gmail.com>
2023-09-30 01:14:15 -0600

    feat: creando el descriptor de aplicacion, la inicioAspirantePage y la detallesSolicitudPage

--- app/restAPIExtensions/solicitudRestAPIExtension/src/main/java/com/company/rest/api/dto/SolicitudResult.java
+++ app/restAPIExtensions/solicitudRestAPIExtension/src/main/java/com/company/rest/api/dto/SolicitudResult.java
@@ -2,13 +2,15 @@ package com.company.rest.api.dto;
 
 public class SolicitudResult {
 
+	private final String persistence_id;
 	private final String status;
 	private final String fecha_inicio;
 	private final String licenciatura_nombre;
 	private final String periodo_de_ingreso_nombre;
 	
-	public SolicitudResult(String status, String fecha_inicio, String licenciatura_nombre, String periodo_de_ingreso_nombre) {
-        this.status = status;
+	public SolicitudResult(String persistence_id, String status, String fecha_inicio, String licenciatura_nombre, String periodo_de_ingreso_nombre) {
+        this.persistence_id = persistence_id;
+		this.status = status;
         this.fecha_inicio = fecha_inicio;
         this.licenciatura_nombre = licenciatura_nombre;
         this.periodo_de_ingreso_nombre = periodo_de_ingreso_nombre;
@@ -28,5 +30,9 @@ public class SolicitudResult {
 
 	public String getPeriodo_de_ingreso_nombre() {
 		return periodo_de_ingreso_nombre;
+	}
+
+	public String getPersistence_id() {
+		return persistence_id;
 	}
 }
